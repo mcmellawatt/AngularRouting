9be2e51663ee5b5c74b56c5c1723306a3de84c1f
Ryan McMahon <mcmellawat@gmail.com>
2016-09-20 12:06:23 -0700

    Lazy loading components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-import { HomeComponent } from './home.component';
-import { ContactsComponent } from './contacts.component';
 import appRoutes from "./app.routes";
 
 @NgModule({
   imports: [ BrowserModule, appRoutes ],
-  declarations: [ AppComponent, HomeComponent, ContactsComponent ],
+  declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
